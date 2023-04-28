57ad8dfa2ebb4b54831643d7df88bc08faa7d336
Killian Colin <killian.colin@proton.me>
2023-04-28 13:31:29 +0200

    [feat] create first version of onde effect

--- src/shaders/test/water_drop.glsl
+++ src/shaders/test/water_drop.glsl
@@ -3,8 +3,24 @@ uniform float uTime;
 
 varying vec2 vUv;
 
+float make_onde(float dist,float creationTime){
+    float waveSize = 0.2;
+    float customTimeDist=uTime/3.0;
+    float customTimeFade=uTime/3.0;
+    float waves = 3.0;
+    float onde= mod(max(dist-customTimeDist,-waveSize*waves),waveSize)/waveSize;
+    //float onde = mod(dist-customTimeDist,waveSize)/waveSize;
+    float centered_onde=onde- 0.5;
+    float result = 1.0-abs(centered_onde)*2.0;
+    float calc = step(0.0,(customTimeDist - creationTime) - dist);
+    float fade = max (1.0- (customTimeFade - creationTime),0.0);
+    return calc*result*fade;
+}
+
 void main()
 {
-    float dist = mod(max(distance(vUv,uCenter)-uTime/10.0,-3.0),0.1)*10.0;
-    gl_FragColor = vec4(dist,0.0,0.0,1.0);
+    float dist = distance(vUv,uCenter);
+    float custom = make_onde(dist,0.0);
+    vec3 color = vec3(vUv, 1.0) * custom;
+    gl_FragColor = vec4(color,1.0);
 }
